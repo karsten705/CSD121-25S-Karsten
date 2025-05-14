fed832dd11aa5b0f69a08f42979bf103434ef506
Karsten Marinus <07028074@saultcollege.ca>
2025-05-14 18:00:49 -0400

    second Commit on lab1 branch

--- src/main/java/lab1/Main.java
+++ src/main/java/lab1/Main.java
@@ -1,6 +1,30 @@
 package lab1;
+
+import java.util.*;
+
 public class Main{
     public static void main(String[] args) {
         System.out.print("Hello, World!");
+
+        int age = 35;
+        String name = "Karsten";
+
+        System.out.println("Age: " + age);
+        System.out.println("Name: " + name);
+
+        if (age > 34) {
+            System.out.println("You're OLD!");
+        }
+        else if (age < 34) {
+            System.out.println("You're still young!");
+        }
+
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Enter your name: ");
+        String userName = scanner.nextLine(); // Reading a string input
+
+        System.out.print("Enter your age: ");
+        int userAge = scanner.nextInt();
     }
 }
